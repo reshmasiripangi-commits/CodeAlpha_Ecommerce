bf58c3dd558e4205cec1ca5c72219a9fc35e5f12
Manasa9920 <manasa.nagilla@nexsysit.co.in>
2026-06-13 12:02:40 +0530

    enchancement

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Product
-# Register your models here.
+from .models import Product, Cart, Order
+
 admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(Order)
--- store/models.py
+++ store/models.py
@@ -4,11 +4,15 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
 class Product(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
